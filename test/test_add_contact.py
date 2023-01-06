ecd9cbc1fc219c93da9039e44d0f754c9c24d653
mukhina-v <mukhina.victoria1993@yandex.ru>
2023-01-06 14:09:53 +0300

    Выделен вспомогательный класс для цправления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,12 +13,12 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Victorya", middlename="Alexandr", lastname="Razumnykh", title="Title",
                                    company="bercut", address="Sennaya", home_phone="7777777",
                                    mobile_phone="89609999999",
                                    work_phone="567", fax="678", email1="mu@ya.ru", email2="victoria@ya.ru",
                                    byear="1993", bday="16", bmonth="January"))
-    app.logout()
+    app.session.logout()
 
 
