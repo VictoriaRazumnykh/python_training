c07f7f4032a419d30d83b8224b822856462bcce1
mukhina-v <mukhina.victoria1993@yandex.ru>
2023-01-06 16:39:03 +0300

    Добавлено редактирование и удаление контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -85,3 +85,28 @@ class ContactHelper:
     def add_new_contact(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
+
+    def edit(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@title='Edit']").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("update").click()
+        self.app.open_home_page()
+
+    def delete(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to.alert.accept()
+        self.app.open_home_page()
---
+++ test/test_edit_contact.py
@@ -0,0 +1,11 @@
+from model.contact import Contact
+
+
+def test_edit_contact(app):
+    app.session.login("admin", "secret")
+    app.contact.edit(Contact(firstname="Harry", middlename="James", lastname="Potter", title="Title",
+                                   company="Hogwarts", address="Sennaya", home_phone="7777777",
+                                   mobile_phone="89609999999",
+                                   work_phone="567", fax="678", email1="mu@ya.ru", email2="victoria@ya.ru",
+                                   byear="1993", bday="16", bmonth="January"))
+    app.session.logout()
