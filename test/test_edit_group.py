6723ef9faa2b1dfd2bb6e73e249c15d1be486790
mukhina-v <mukhina.victoria1993@yandex.ru>
2023-01-15 15:01:43 +0300

    Реализовано кеширование для методов, загружающих списки групп.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -7,8 +7,8 @@ def test_edit_first_group(app):
     group = Group(name="Harry", header="Potter", footer="wizard")
     group.id = old_groups[0].id
     app.group.edit(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
@@ -20,8 +20,9 @@ def test_edit_first_group_name(app):
     group = Group(name="Harry Potter")
     group.id = old_groups[0].id
     app.group.edit(group)
+    #new_groups = app.group.get_group_list()
+    assert len(old_groups) == app.group.count() #len(new_groups)
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
