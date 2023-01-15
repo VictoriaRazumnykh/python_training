45232b2c268146802f1bd04c89f6a28e466da625
mukhina-v <mukhina.victoria1993@yandex.ru>
2023-01-15 13:31:21 +0300

    Доработаны проверки для удаления групп

--- model/group.py
+++ model/group.py
@@ -1,6 +1,16 @@
 class Group:
 
-    def __init__(self, name=None, header=None, footer=None):
+    def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
         self.header = header
         self.footer = footer
+        self.id = id
+
+# определяем как будет выглядеть объект при выводе на консоль (какое его строковое представление)
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+# тк питон считает объекты разными из-за физического расположения, определяем ф-цию, которая реализует логическое сравнение
+#чтобы сравнение выполнялось по смыслу
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,22 +3,36 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(name="Harry", header="Potter", footer="wizard"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(name="Harry Potter"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(header="wizard"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(footer="Potter"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
+
