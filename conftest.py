c60e14a8e519145812963cf27b978bc739b16b77
mukhina-v <mukhina.victoria1993@yandex.ru>
2023-01-24 19:25:08 +0300

    Создан конфигурационный файл target.json

--- conftest.py
+++ conftest.py
@@ -1,23 +1,23 @@
 import pytest
 from fixture.application import Application
+import json
 
 fixture = None
+target=None
 
 
 # функция, которая инициализирует фикстуру
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    user = request.config.getoption("--user")
-    password = request.config.getoption("--password")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url, user=user, password=password)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url, user=user, password=password)
-    fixture.session.ensure_login(user, password)
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"], user=target["username"], password=target["password"])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -33,6 +33,8 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--user", action="store", default="admin")
-    parser.addoption("--password", action="store", default="secret")
+    parser.addoption("--target", action="store", default="target.json")
+
+    #parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    #parser.addoption("--user", action="store", default="admin")
+    #parser.addoption("--password", action="store", default="secret")
