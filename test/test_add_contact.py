dd6463025c85b5b1724fba206cedec956395eb23
mukhina-v <mukhina.victoria1993@yandex.ru>
2023-01-26 23:32:57 +0300

    Реализована загрузка тестовых данных из файлов в формате JSON и параметризация тестов путём "внедрения" этих тестовых данных в фикстуру при помощи hook-функции pytest_generate_tests.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -9,3 +9,13 @@ def test_add_contact(app, data_contacts):
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+def test_add_json_contact(app, json_contacts):
+    contact = json_contacts
+    old_contacts = app.contact.get_contact_list()
+    app.contact.create_contact(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -15,6 +15,17 @@ def test_add_group(app, data_groups):
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
+
+def test_add_json_group(app, json_groups):
+    group = json_groups
+    old_groups = app.group.get_group_list()
+    # group = Group(name="mmm", header="fggffhhg", footer="fhfghhgng")
+    app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
+    new_groups = app.group.get_group_list()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 # def test_add_empty_group(app):
 #    old_groups = app.group.get_group_list()
 #    group = Group(name="", header="", footer="")
