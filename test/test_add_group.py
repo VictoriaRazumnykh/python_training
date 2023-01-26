529262bf8a5284d1038f0a00821aa7df90e02c85
mukhina-v <mukhina.victoria1993@yandex.ru>
2023-01-26 20:43:33 +0300

    Добавлен новый пакет с данными для создания группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,25 +1,8 @@
 from model.group import Group
 import pytest
-import random
-import string
-
-# генерируем случайные тестовые данные
-def random_string(prefix, maxlen):
-    # символы из случайно сгенерированной строки (*10 - частота встречающихся пробелов)
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    # гененируем случайную строку
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-        Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
-        for i in range(5)
-]
-#testdata = [Group(name=name, header=header, footer=footer)
-#        for name in ["", random_string("name", 10)]
-#        for header in ["", random_string("header", 10)]
-#        for footer in ["", random_string("footer", 10)]
-#        ]
+from data.add_group import testdata
+# или для использования фиксированных значений:
+#from data.add_group import constant as testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
